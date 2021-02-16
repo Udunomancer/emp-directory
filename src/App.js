f458b49a4325c5a3ab85ed85ac6e2a398eb1967e
Kyle Chriscoe <kyle717@gmail.com>
2021-02-16 16:37:24 -0500

    added the Directory page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import './App.css';
-import Navbar from "./components/Navbar/Navbar";
+import Directory from "./pages/Directory/Directory";
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <div className="container">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </>
+    <Directory/>
   );
 }
 
---
+++ src/pages/Directory/Directory.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+
+function Directory() {
+    return (
+        <>
+            <Navbar/>
+        </>
+    )
+}
+
+export default Directory;
