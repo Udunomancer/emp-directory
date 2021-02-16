f97aebb90b341bd43bde61631bfc2ab4d562062f
Kyle Chriscoe <kyle717@gmail.com>
2021-02-16 18:35:23 -0500

    added container component

--- src/pages/Directory/Directory.jsx
+++ src/pages/Directory/Directory.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import Container from "../../components/Container/Container";
 import UsersAPI from "../../utils/UsersAPI/UsersAPI";
 
 function Directory() {
@@ -7,18 +8,21 @@ function Directory() {
     const [usersState, setUsersState] = useState({});
     
 
-    useEffect(() => {
-        UsersAPI.getRandomUsers(25)
-          .then(res => {
-              setUsersState(res);
-              console.log("Users State:");
-              console.log(usersState);
-            });
-    }, []);
+    // useEffect(() => {
+    //     UsersAPI.getRandomUsers(25)
+    //       .then(res => {
+    //           setUsersState(res);
+    //           console.log("Users State:");
+    //           console.log(usersState);
+    //         });
+    // }, []);
 
     return (
         <>
             <Navbar/>
+            <Container>
+                <p>This is inside the container.</p>
+            </Container>
         </>
     )
 }
