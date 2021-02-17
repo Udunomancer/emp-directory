447116aeb93837ac6f3dc155a4559ca0a8673008
Kyle Chriscoe <kyle717@gmail.com>
2021-02-17 16:52:33 -0500

    added listeners for all search fields

--- src/components/UserTable/UserTable.jsx
+++ src/components/UserTable/UserTable.jsx
@@ -1,9 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import UserRow from "../UserRow/UserRow";
 import SortButton from "../SortButton/SortButton";
 
 function UserTable({ users, sort, handleInputChange, searchState }) {
-  
   const [showSearch, setShowSearch] = useState(false);
 
   function switchVisibility() {
@@ -15,38 +14,48 @@ function UserTable({ users, sort, handleInputChange, searchState }) {
       <thead>
         <tr>
           <th>
-            <button 
-              className="btn fas fa-search" 
-              value={false} 
+            <button
+              className="btn fas fa-search"
+              value={false}
               id="searchToggle"
               onClick={switchVisibility}
             ></button>
           </th>
-          
+
           <th>
             <div>
               Name
               <SortButton id="name" value="down" action={sort} />
               <SortButton id="name" value="up" action={sort} />
             </div>
-          {showSearch &&
-            <div>
-              <input type="text" name="nameSearch" value={searchState.nameSearch} onChange={handleInputChange}></input>
-            </div>
-          }
+            {showSearch && (
+              <div>
+                <input
+                  type="text"
+                  name="nameSearch"
+                  value={searchState.nameSearch}
+                  onChange={handleInputChange}
+                ></input>
+              </div>
+            )}
           </th>
-          
+
           <th>
             <div>
               Phone
               <SortButton id="phone" value="down" action={sort} />
               <SortButton id="phone" value="up" action={sort} />
             </div>
-            {showSearch &&
-            <div>
-              <input type="text" ></input>
-            </div>
-}
+            {showSearch && (
+              <div>
+                <input
+                  type="text"
+                  name="phoneSearch"
+                  value={searchState.phoneSearch}
+                  onChange={handleInputChange}
+                ></input>
+              </div>
+            )}
           </th>
           <th>
             <div>
@@ -54,11 +63,16 @@ function UserTable({ users, sort, handleInputChange, searchState }) {
               <SortButton id="email" value="down" action={sort} />
               <SortButton id="email" value="up" action={sort} />
             </div>
-            {showSearch &&
-            <div>
-              <input type="text"></input>
-            </div>
-}
+            {showSearch && (
+              <div>
+                <input 
+                  type="text"
+                  name="emailSearch"
+                  value={searchState.emailSearch}
+                  onChange={handleInputChange}
+                ></input>
+              </div>
+            )}
           </th>
           <th>
             <div>
@@ -66,11 +80,16 @@ function UserTable({ users, sort, handleInputChange, searchState }) {
               <SortButton id="address" value="down" action={sort} />
               <SortButton id="address" value="up" action={sort} />
             </div>
-            {showSearch &&
-            <div>
-              <input type="text"></input>
-            </div>
-}
+            {showSearch && (
+              <div>
+                <input 
+                  type="text"
+                  name="addressSearch"
+                  value={searchState.addressSearch}
+                  onChange={handleInputChange}
+                ></input>
+              </div>
+            )}
           </th>
         </tr>
       </thead>
