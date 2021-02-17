f0d790fcf0d63c149e7d286376053ea4b144da4d
Kyle Chriscoe <kyle717@gmail.com>
2021-02-17 18:21:39 -0500

    navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Navbar() {
-    return (
-        <nav className="navbar navbar-light bg-light">
-            <h1>This is the Navbar</h1>
-        </nav>
-    )
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Employee Directory</span>
+        <span class="navbar-text">
+          Find employee contact information by searching below
+        </span>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
