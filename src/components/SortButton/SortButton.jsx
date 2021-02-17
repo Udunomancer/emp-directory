8758e94d56762d6e3faf7396eb010a677c8103a8
Kyle Chriscoe <kyle717@gmail.com>
2021-02-16 23:13:02 -0500

    reorganized SortButton for ease of use

--- src/components/SortButton/SortButton.jsx
+++ src/components/SortButton/SortButton.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function SortButton(props) {
     return (
-        <button className="btn p-1" id={props.id} value={props.value} onClick={props.action}>
-            <i className={`fas fa-angle-${props.value}`}></i>
+        <button 
+          className={`btn p-1 fas fa-angle-${props.value}`} 
+          id={props.id} 
+          value={props.value} 
+          onClick={props.action}>
         </button>
     )
 }
